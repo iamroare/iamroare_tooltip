1438c6ebf74c25eb415b96a14949c3f544f10880
Rahul Prajapati <95571941+iamroare@users.noreply.github.com>
2023-01-27 10:13:11 +0530

    this is my tooltip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import Tooltip from "./tooltip";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// APP COMPONENT
+class App extends React.Component{
+
+
+  constructor(){
+    super();
+    this.state ={
+      positoin: "top",
+      hovering: false
+    };
+  }
+
+  // FUNCTIONALITY FOR HANLDECLICK
+  handleClick = (pos) =>{
+    this.setState({
+      position: pos
+    })
+  };
+
+  // FUNCTIONALITY FOR HANDLE_MOUSE_ENTER
+  handleMouseEnter = () =>{
+    this.setState({hovering: true});
+  };
+
+  // FUNCTIONALITY FOR HANDLE_MOUSE_LEAVE
+  handleMouseLeave = () =>{
+    this.setState({hovering: false});
+  };
+
+
+
+  render(){
+    return (
+      <div  className="App">
+        <div className="btn-position">
+          <div className="grp-name">
+            Select the position for hovering
+          </div>
+                    {/* BUTTONS WHICH WILL APPEAR ON TOP */}
+          <button
+          className={this.state.position === "top" ? "btn active" : "btn"}
+          onClick={(e)=>{this.handleClick("top")}}
+          >
+            Top
+          </button>
+
+          <button
+          className={this.state.position === "left" ? "btn active" : "btn"}
+          onClick={(e)=>{this.handleClick("left")}}>
+            Left
+          </button>
+
+          <button
+          className={this.state.position === "right" ? "btn active" : "btn"}
+          onClick={(e)=>{this.handleClick("right")}}>
+            Right
+          </button>
+
+          <button
+          className={this.state.position === "down" ? "btn active" : "btn"}
+          onClick={(e)=>{this.handleClick("down")}}>
+            Bottom
+          </button>
+        </div>
+
+        {/* BUTTON CONTAINER WHICH WILL BE USED FOR HOVERING */}
+        <div id="button-container">
+          <button
+          className="btn hover-btn"
+          onMouseOver={this.handleMouseEnter}
+          onMouseOut={this.handleMouseLeave}
+          >
+            Hover Over Me!!!
+          </button>
+          {this.state.hovering && <Tooltip position={this.state.position}  />}
+        </div>
+
+          {/* FOR SHOWING COPYRIGHTS YEAH!!! */}
+        <div>
+          <h5 className="copyright">
+            Copyright 2023 iamROARe. All Rights Reserved!
+          </h5>
+        </div>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+export
+ default App;
